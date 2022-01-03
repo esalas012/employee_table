5ed3959f3426cbc42dea3fefac6fc624c63b25d6
Eddie Salas <edusalas92@hotmail.com>
2022-01-02 23:45:48 -0500

    Changed stored data

--- employee-table-backend/employee_list.js
+++ employee-table-backend/employee_list.js
@@ -77,71 +77,71 @@ exports.employeeList = {
         id: '394e8f8e-6976-11ec-90d6-0242ac120003'
     },
     '394e90ec-6976-11ec-90d6-0242ac120003': {
-        firstName: "Tom",
-        lastName: "Smith",
+        firstName: "Jae",
+        lastName: "Pa",
         hireDate: "2001-03-05",
         role: "VP",
         id: '394e90ec-6976-11ec-90d6-0242ac120003'
     },
     '394e9218-6976-11ec-90d6-0242ac120003': {
-        firstName: "Alfred",
-        lastName: "Hong",
+        firstName: "Elieser",
+        lastName: "Garcia",
         hireDate: "2012-12-12",
         role: "Manager",
         id: '394e9218-6976-11ec-90d6-0242ac120003'
     },
     '394e9326-6976-11ec-90d6-0242ac120003': {
-        firstName: "Maria",
-        lastName: "Fuentes",
+        firstName: "Adrian",
+        lastName: "Hernandez",
         hireDate: "2005-09-10",
         role: "CEO",
         id: '394e9326-6976-11ec-90d6-0242ac120003'
     },
     '394e9786-6976-11ec-90d6-0242ac120003': {
-        firstName: "Tom",
+        firstName: "Mary",
         lastName: "Smith",
         hireDate: "2001-03-05",
         role: "VP",
         id: '394e9786-6976-11ec-90d6-0242ac120003'
     },
     '394e98da-6976-11ec-90d6-0242ac120003': {
-        firstName: "Alfred",
-        lastName: "Hong",
+        firstName: "Keith",
+        lastName: "Bing",
         hireDate: "2012-12-12",
         role: "Manager",
         id: '394e98da-6976-11ec-90d6-0242ac120003'
     },
     '394e99f2-6976-11ec-90d6-0242ac120003': {
-        firstName: "Maria",
+        firstName: "Joey",
         lastName: "Fuentes",
         hireDate: "2005-09-10",
         role: "CEO",
         id: '394e99f2-6976-11ec-90d6-0242ac120003'
     },
     '394e9b0a-6976-11ec-90d6-0242ac120003': {
-        firstName: "Tom",
-        lastName: "Smith",
+        firstName: "Dustin",
+        lastName: "Jonas",
         hireDate: "2001-03-05",
         role: "VP",
         id: '394e9b0a-6976-11ec-90d6-0242ac120003'
     },
     '394e9d58-6976-11ec-90d6-0242ac120003': {
-        firstName: "Alfred",
-        lastName: "Hong",
+        firstName: "Michael",
+        lastName: "Bouble",
         hireDate: "2012-12-12",
         role: "Manager",
         id: '394e9d58-6976-11ec-90d6-0242ac120003'
     },
     '394e9ee8-6976-11ec-90d6-0242ac120003': {
-        firstName: "Maria",
-        lastName: "Fuentes",
+        firstName: "Jason",
+        lastName: "Marx",
         hireDate: "2005-09-10",
         role: "CEO",
         id: '394e9ee8-6976-11ec-90d6-0242ac120003'
     },
     '394ea050-6976-11ec-90d6-0242ac120003': {
-        firstName: "Tom",
-        lastName: "Smith",
+        firstName: "Connor",
+        lastName: "Patel",
         hireDate: "2001-03-05",
         role: "VP",
         id: '394ea050-6976-11ec-90d6-0242ac120003'
